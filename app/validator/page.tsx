10ab79fded4764c549c77c462237d4507a96382f
Micka <mickael.murmann@gmail.com>
2024-10-01 18:46:59 +0200

    Delete title

--- app/validator/page.tsx
+++ app/validator/page.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 const ControlPage = () => {
     return (
         <div className="container mx-auto py-8">
-            <h1 className="text-3xl font-bold mb-8">Contrôle des billets</h1>
             {/* Intégration du composant TicketValidator */}
             <TicketValidator />
         </div>
