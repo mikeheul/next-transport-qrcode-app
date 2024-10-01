be9a9c95393578637d349c3451f16bd4999d0fce
Micka <mickael.murmann@gmail.com>
2024-10-01 21:50:42 +0200

    Fix Header component (icons)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,30 @@
 "use client";
 
 import Link from 'next/link';
+import { QrCode, CheckCircle } from 'lucide-react'; // Import the necessary icons
 
 const Header = () => {
     return (
         <header className="bg-slate-900 px-10 py-8 shadow-md">
             <div className="container mx-auto flex justify-between items-center">
                 <h1 className="text-2xl font-bold text-white">Billetterie</h1>
-                <nav className="mt-2">
-                    <ul className="flex space-x-6">
+                <nav>
+                    <ul className="flex items-center justify-center space-x-6">
                         <li>
                             <Link
                                 href="/qrcode"
-                                className="relative text-white hover:text-blue-400 transition duration-200"
+                                className="relative text-white hover:text-blue-400 transition duration-200 flex items-center"
                             >
-                                Générer Billet
+                                <QrCode className="w-8 h-8 mr-2" /> {/* QR Code Icon */}
                                 <span className="absolute inset-x-0 bottom-0 h-0.5 bg-blue-400 transform scale-x-0 transition-all duration-200 group-hover:scale-x-100" />
                             </Link>
                         </li>
                         <li>
                             <Link
                                 href="/validator"
-                                className="relative text-white hover:text-blue-400 transition duration-200"
+                                className="relative text-white hover:text-blue-400 transition duration-200 flex items-center"
                             >
-                                Valider Billet
+                                <CheckCircle className="w-8 h-8 mr-2" /> {/* Check Circle Icon */}
                                 <span className="absolute inset-x-0 bottom-0 h-0.5 bg-blue-400 transform scale-x-0 transition-all duration-200 group-hover:scale-x-100" />
                             </Link>
                         </li>
